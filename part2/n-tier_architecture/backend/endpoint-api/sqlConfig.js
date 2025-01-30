80f200ff7019c0a15306c300d945f1e9b39cb404
pimpa <pimpamaree@gmail.com>
2025-01-30 21:35:42 +0700

    chroic: Change code

--- part2/n-tier_architecture/backend/endpoint-api/sqlConfig.js
+++ part2/n-tier_architecture/backend/endpoint-api/sqlConfig.js
@@ -1,8 +1,8 @@
 var dbconfig = {
     development: {
-        server: '192.168.56.106',
-        database:'team0_engce301_db',
-        user:'team0',
+        server: '192.168.52.130',
+        database:'team3_engce301_db',
+        user:'team3',
         password:'P@ssw0rd',
         port: 1433,
         options:{
@@ -11,7 +11,7 @@ var dbconfig = {
             enableArithAbort: true,
             trustServerCertificate: true,
             trustedconnection:  true,
-            instancename:  '192.168.56.106'  // SQL Server instance name
+            instancename:  '192.168.52.130'  // SQL Server instance name
         }
     },
     production: {
